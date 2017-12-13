59e0a1a5edb71c51899fdae009011fb9fd35db96
PROD\mqorinna <martha.qorinna@alten.nl>
2017-12-13 14:34:26 +0100

    added class diagram png

--- CodeCoverage/ExecuteTest.sh
+++ CodeCoverage/ExecuteTest.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+cd ../Pi/build/test
+./KYPHSensors_test
+./ProtocolUnwrapper_test
+./SQLQuery_test
+./SerialDataHandler_test
 
+cd ../../../
+
+lcov -c -d . -o CodeCoverage/Pi/kyph_pi.info
+
+genhtml Pi/kyph_pi.info
